d1bdae373cd01ac41a4b3729b399df7379b69eab
Hannes Rüger <hannesrueger@gmx.de>
2019-11-29 16:27:24 -1000

    fixed studienseminar and updated to bs 4

--- bestenliste_standalone.php
+++ bestenliste_standalone.php
@@ -48,27 +48,18 @@ require_once("./include/db.inc.php");
 			<?php if (isset($_GET["s"])) { ?>
 				<h3>Die besten 10 Schüler</h3>
 			<?php
-				createBestenliste("SELECT * FROM users WHERE grade NOT IN ('Lehrer/in', 'Studienseminar 17/19', 'Studienseminar 18/20')  AND `checked`!=-1 AND `hideInScores`!=1 ORDER BY `points` DESC LIMIT 10", false);
+				createBestenliste("SELECT * FROM users WHERE grade NOT IN ('Lehrer/in', 'Studienseminar 19/21', 'Studienseminar 18/20')  AND `checked`!=-1 AND `hideInScores`!=1 ORDER BY `points` DESC LIMIT 10", false);
 			} else if (isset($_GET["l"])) { ?>
 				<h3>Die besten 10 Lehrer</h3>
 				<?php
-					createBestenliste("SELECT * FROM users WHERE grade IN ('Lehrer/in', 'Studienseminar 17/19', 'Studienseminar 18/20')  AND `checked`!=-1 AND `hideInScores`!=1 ORDER BY `points` DESC LIMIT 10", false);
+					createBestenliste("SELECT * FROM users WHERE grade IN ('Lehrer/in', 'Studienseminar 19/21', 'Studienseminar 18/20')  AND `checked`!=-1 AND `hideInScores`!=1 ORDER BY `points` DESC LIMIT 10", false);
 					?>
 			<?php } ?>
-
-
-
-
-
 			<div class="clearfix">&nbsp;</div>
-
 		</div>
-		<br>
-		<div class="card">
+		<div class="card p-2 text-center">
 			<h4><?php echo "Stand: " . strftime("%A") . ", " . date('d.m.o \u\m H:i:s') . " Uhr"; ?></h4>
 		</div>
-
-
 		<?php echo getFooter(); ?>
 
 	</div>
