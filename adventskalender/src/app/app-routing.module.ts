a623af825a8cb35802da56943b4250ad206aaf70
Hannes Rüger <hannesrueger@gmx.de>
2020-11-01 17:41:56 +0100

    (frontend) added some routes

--- adventskalender/src/app/app-routing.module.ts
+++ adventskalender/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./_pages/home/home.component";
+import { LoginComponent } from "./_pages/login/login.component";
+import { RegisterComponent } from "./_pages/register/register.component";
 
 const routes: Routes = [
-    { path: "", component: HomeComponent },
+    { path: "home", component: HomeComponent },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "**", redirectTo: "home", pathMatch: "full" },
 ];
 
 @NgModule({
