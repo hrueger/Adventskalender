15ae67cfb705cba949c7189900d74a1f2347c98a
Hannes Rüger <hannesrueger@gmx.de>
2020-11-01 17:15:27 +0100

    created components + removed demo content

--- adventskalender/src/app/app.module.ts
+++ adventskalender/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { LoginComponent } from './_pages/login/login.component';
+import { RegisterComponent } from './_pages/register/register.component';
+import { ScoresComponent } from './_pages/scores/scores.component';
+import { RulesComponent } from './_pages/rules/rules.component';
+import { HomeComponent } from './_pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ScoresComponent,
+    RulesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
