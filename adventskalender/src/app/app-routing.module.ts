405b55c6925bf648a798be67645ffbef3e5e397a
Hannes Rüger <hannesrueger@gmx.de>
2020-11-03 10:27:36 +0100

    (frontend) added routes to scores and rules

--- adventskalender/src/app/app-routing.module.ts
+++ adventskalender/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./_pages/home/home.component";
 import { LoginComponent } from "./_pages/login/login.component";
 import { RegisterComponent } from "./_pages/register/register.component";
+import { RulesComponent } from "./_pages/rules/rules.component";
+import { ScoresComponent } from "./_pages/scores/scores.component";
 
 const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
+    { path: "scores", component: ScoresComponent },
+    { path: "rules", component: RulesComponent },
     { path: "**", redirectTo: "home", pathMatch: "full" },
 ];
 
