8f8f101231739c4e54252ca4ce49aab4ea2e6276
Hannes Rüger <hannesrueger@gmx.de>
2020-11-03 14:00:49 +0100

    (api) fixed leftover route

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController";
-import { checkJwt } from "../middlewares/checkJwt";
 
 const router = Router();
 // Login route
 router.post("/login", AuthController.login);
-router.post("/password", [checkJwt], AuthController.changePassword);
 router.post("/renewToken", AuthController.renewToken);
 
 export default router;
