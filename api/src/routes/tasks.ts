1e3935bcaa74dd60d481aec825966c65850068c6
H. Rüger <hannesrueger@gmx.de>
2020-11-12 12:26:41 +0100

    fixed missing login check for tasks route

--- api/src/routes/tasks.ts
+++ api/src/routes/tasks.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import TasksController from "../controllers/TasksController";
+import { checkJwt } from "../middlewares/checkJwt";
 
 const router = Router();
-router.get("/", TasksController.listAll);
+router.get("/", [checkJwt], TasksController.listAll);
 
 export default router;
