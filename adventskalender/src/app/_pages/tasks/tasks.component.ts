79cc9e7a92159da10f1f91574a7f0f6c0528dec7
Hannes Rüger <hannesrueger@gmx.de>
2020-11-15 12:14:46 +0100

    (frontend) updated UI

--- adventskalender/src/app/_pages/tasks/tasks.component.ts
+++ adventskalender/src/app/_pages/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Task } from "../../_models/Task";
 import { RemoteService } from "../../_services/remote.service";
 
 @Component({
@@ -7,11 +8,34 @@ import { RemoteService } from "../../_services/remote.service";
     styleUrls: ["./tasks.component.scss"],
 })
 export class TasksComponent {
-    tasks: any = [];
+    tasks: Task[] = [];
+    tasksInRandomOrder: Task[] = [];
     constructor(private remoteService: RemoteService) { }
     public ngOnInit(): void {
-        this.remoteService.get("tasks").subscribe((t) => {
+        this.remoteService.get("tasks").subscribe((t: Task[]) => {
             this.tasks = t;
+            // eslint-disable-next-line no-use-before-define
+            this.tasksInRandomOrder = shuffle([...t]);
         });
     }
+}
+
+/* taken from https://stackoverflow.com/a/2450976/13485777 */
+function shuffle(array) {
+    let currentIndex = array.length; let temporaryValue; let
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
