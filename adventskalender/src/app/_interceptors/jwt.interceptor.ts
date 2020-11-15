04630cc1902ed35505213e256e81620b069a5173
Hannes Rüger <hannesrueger@gmx.de>
2020-11-15 10:23:58 +0100

    fixed authentication

--- adventskalender/src/app/_interceptors/jwt.interceptor.ts
+++ adventskalender/src/app/_interceptors/jwt.interceptor.ts
@@ -3,19 +3,20 @@ import {
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { AuthenticationService } from "../_services/authentication.service";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+    constructor(private authenticationService: AuthenticationService) { }
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let token = sessionStorage.getItem("jwt_token");
-        if (!token) {
-            token = "";
+        if (this.authenticationService.currentUser) {
+            const authReq = req.clone({
+                headers: new HttpHeaders({
+                    authorization: this.authenticationService.currentUser?.jwtToken,
+                }),
+            });
+            return next.handle(authReq);
         }
-        const authReq = req.clone({
-            headers: new HttpHeaders({
-                authorization: token,
-            }),
-        });
-        return next.handle(authReq);
+        return next.handle(req);
     }
 }
