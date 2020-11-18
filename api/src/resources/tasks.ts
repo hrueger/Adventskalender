ccab4db1054c9cf9cb563a6f6af539a0b6181f6b
Hannes Rüger <hannesrueger@gmx.de>
2020-11-18 13:24:31 +0100

    (api) added correct task data

--- api/src/resources/tasks.ts
+++ api/src/resources/tasks.ts
@@ -1,48 +1,268 @@
 import { Task } from "../entity/Task";
 
-export const tasks: Task[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map((n) => ({
-    day: n,
-    young: {
-        description: `Test am Tag ${n} leicht`,
-        solutions: [
-            {
-                row: 5,
-                col: "E",
-            },
-            {
-                row: 6,
-                col: "E",
-            },
-            {
-                row: 7,
-                col: "E",
-            },
-            {
-                row: 8,
-                col: "E",
-            },
-        ],
+export const tasks: Task[] = [
+    {
+        day: 1,
+        young: {
+            description: "Seitenblick",
+            solutions: [{ col: "E", row: 5 }],
+        },
+        old: {
+            description: "Zwei blaue Luftikusse sind selten allein.",
+            solutions: [{ col: "I", row: 2 }],
+        },
     },
-    old: {
-        description: `Test am Tag ${n} schwer`,
-        solutions: [
-            {
-                row: 5,
-                col: "E",
-            },
-            {
-                row: 6,
-                col: "E",
-            },
-            {
-                row: 7,
-                col: "E",
-            },
-            {
-                row: 8,
-                col: "E",
-            },
-        ],
+    {
+        day: 2,
+        young: {
+            description: "Geht dir ein Licht auf?",
+            solutions: [{ col: "E", row: 5 }, { col: "E", row: 6 }],
+        },
+        old: {
+            description: "Ein rumhängendes bemaltes Osterei? Denk mal ans Abitur vor einigen Jahren!",
+            solutions: [{ col: "D", row: 7 }],
+        },
     },
-}));
+    {
+        day: 3,
+        young: {
+            description: "Tablets ohne WLAN-Anschluss",
+            solutions: [{ col: "F", row: 5 }, { col: "F", row: 6 }],
+        },
+        old: {
+            description: "Kurvenreiche Holzarbeit: Gedacht für Gläser, Tassen und Blumentöpfe, aber nicht für die Füße!",
+            solutions: [{ col: "F", row: 5 }],
+        },
+    },
+    {
+        day: 4,
+        young: {
+            description: "Aufwachen!",
+            solutions: [{ col: "G", row: 2 }],
+        },
+        old: {
+            description: "Gleichseitige Dreiecke am AG",
+            solutions: [{ col: "H", row: 4 }],
+        },
+    },
+    {
+        day: 5,
+        young: {
+            description: "Hier ziehen die Schüler ihre Kreise.",
+            solutions: [{ col: "F", row: 7 }],
+        },
+        old: {
+            description: "Baumlochschnecke?",
+            solutions: [{ col: "D", row: 2 }],
+        },
+    },
+    {
+        day: 6,
+        young: {
+            description: "Seilt sich hier in der Biologie eine schwarze Mamba ab?",
+            solutions: [{ col: "C", row: 5 }],
+        },
+        old: {
+            description: "Eine Thermoskanne für Picknicks auf flachen Dächern?",
+            solutions: [{ col: "E", row: 3 }],
+        },
+    },
+    {
+        day: 7,
+        young: {
+            description: "Sprudelnde oder stille Kunst?",
+            solutions: [{ col: "F", row: 5 }],
+        },
+        old: {
+            description: "Hier gibts die höchsten Töne.",
+            solutions: [{ col: "G", row: 1 }],
+        },
+    },
+    {
+        day: 8,
+        young: {
+            description: "Ein farbenfroher Adventskalender?",
+            solutions: [{ col: "G", row: 3 }],
+        },
+        old: {
+            description: "Glänzende Einblicke aus westlicher Richtung",
+            solutions: [{ col: "G", row: 5 }],
+        },
+    },
+    {
+        day: 9,
+        young: {
+            description: "Ein Abakus zum Rechnen? Am AG gibt es doch Taschenrechner!",
+            solutions: [{ col: "E", row: 7 }],
+        },
+        old: {
+            description: "Wer geht denn gleich mit dem Kopf durch die Wand - zudem so kunstvoll?",
+            solutions: [{ col: "B", row: 4 }],
+        },
+    },
+    {
+        day: 10,
+        young: {
+            description: "Schau nicht so traurig, bald ist Weihnachten!",
+            solutions: [{ col: "E", row: 6 }],
+        },
+        old: {
+            description: "Eine Spaßbadrutsche am AG?",
+            solutions: [{ col: "F", row: 6 }],
+        },
+    },
+    {
+        day: 11,
+        young: {
+            description: "Hier begrüßt das Grün.",
+            solutions: [{ col: "E", row: 5 }],
+        },
+        old: {
+            description: "Schraube locker? Nicht am AG!",
+            solutions: [{ col: "B", row: 9 }],
+        },
+    },
+    {
+        day: 12,
+        young: {
+            description: "Plan einer Weggabelung?",
+            solutions: [{ col: "E", row: 5 }],
+        },
+        old: {
+            description: "Schau! Shawn das Schaf grast grünes Gras.",
+            solutions: [{ col: "C", row: 8 }, { col: "D", row: 8 }],
+        },
+    },
+    {
+        day: 13,
+        young: {
+            description: "Am AG hängen so mache rum.",
+            solutions: [{ col: "E", row: 6 }],
+        },
+        old: {
+            description: "Sehr nahe, aber übersieht man leicht in Gebüsch",
+            solutions: [{ col: "I", row: 1 }],
+        },
+    },
+    {
+        day: 14,
+        young: {
+            description: "Zum Fürchten? Eher zum Lachen!",
+            solutions: [{ col: "G", row: 8 }],
+        },
+        old: {
+            description: "Kommt fast täglich was rein. Halb drinnen, halb draußen.",
+            solutions: [{ col: "E", row: 5 }],
+        },
+    },
+    {
+        day: 15,
+        young: {
+            description: "Eine Eisskulptur am AG?",
+            solutions: [{ col: "E", row: 1 }],
+        },
+        old: {
+            description: "Es ranken sich Gerüchte, dass man hier meterlange Steaks grillen kann.",
+            solutions: [{ col: "G", row: 4 }],
+        },
+    },
+    {
+        day: 16,
+        young: {
+            description: "Symmetrische Figuren, nicht nur in Mathe, auch bei der OGS",
+            solutions: [{ col: "C", row: 6 }],
+        },
+        old: {
+            description: "Schau es dir doch von der Nähe an - aber schwindle nicht!",
+            solutions: [{ col: "H", row: 6 }],
+        },
+    },
+    {
+        day: 17,
+        young: {
+            description: "Wir warten darauf.",
+            solutions: [{ col: "F", row: 5 }],
+        },
+        old: {
+            description: "Glänzende Zeugnisse großer Triumphe auf dem quadratischen Spielfeld. Hat aber nichts mit Christiano Ronaldo zu tun!",
+            solutions: [{ col: "C", row: 7 }],
+        },
+    },
+    {
+        day: 18,
+        young: {
+            description: "Lass dich doch nicht hängen!",
+            solutions: [{ col: "E", row: 6 }],
+        },
+        old: {
+            description: "Ein Eck zum Ostereier verstecken",
+            solutions: [{ col: "D", row: 5 }],
+        },
+    },
+    {
+        day: 19,
+        young: {
+            description: "Globusse, Globussis oder Globen?",
+            solutions: [{ col: "D", row: 5 }, { col: "E", row: 5 }],
+        },
+        old: {
+            description: "Was ist hier wohl angekettet, um nicht wegzulaufen zu können?",
+            solutions: [{ col: "G", row: 2 }],
+        },
+    },
+    {
+        day: 20,
+        young: {
+            description: "Bröckelt hier der Putz ab?",
+            solutions: [{ col: "G", row: 5 }],
+        },
+        old: {
+            description: "Was für ein Stangensalat!",
+            solutions: [{ col: "I", row: 2 }],
+        },
+    },
+    {
+        day: 21,
+        young: {
+            description: "Wer wird denn gleich so schreien in der staden Weihnachtszeit!",
+            solutions: [{ col: "E", row: 6 }],
+        },
+        old: {
+            description: "Schau in den Kasten: Ein versteinerter Blumenkohl aus der Schulküche?",
+            solutions: [{ col: "F", row: 8 }],
+        },
+    },
+    {
+        day: 22,
+        young: {
+            description: "Ein treues Pferd oder ein trojanisches Pferd?",
+            solutions: [{ col: "D", row: 5 }],
+        },
+        old: {
+            description: "Welches gefährliche Tier lauert hinter dem Gitter?",
+            solutions: [{ col: "C", row: 4 }],
+        },
+    },
+    {
+        day: 23,
+        young: {
+            description: "Froh nach Farben getrennt",
+            solutions: [{ col: "G", row: 5 }, { col: "H", row: 5 }],
+        },
+        old: {
+            description: "Die Krone eines der heiligen drei Könige? Oder kommt dir da was spanisch vor?",
+            solutions: [{ col: "F", row: 5 }, { col: "F", row: 6 }],
+        },
+    },
+    {
+        day: 22,
+        young: {
+            description: "Kugeliges Wohnzimmer",
+            solutions: [{ col: "D", row: 2 }],
+        },
+        old: {
+            description: "Der Stern von Bethlehem? Oder ein NATO-Geheimzeichen?",
+            solutions: [{ col: "D", row: 6 }],
+        },
+    },
+];
