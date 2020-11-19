e9c6761837aa86792973276fd2121b223b7bb2b5
Hannes Rüger <hannesrueger@gmx.de>
2020-11-19 14:57:43 +0100

    (api) added UI for test mode force date

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import auth from "./auth";
 import users from "./users";
 import tasks from "./tasks";
@@ -8,5 +8,20 @@ const routes = Router();
 routes.use("/auth", auth);
 routes.use("/users", users);
 routes.use("/tasks", tasks);
+routes.get("/testmode", (req: Request, res: Response) => {
+    if (req.app.locals.config.TEST_MODE) {
+        res.send(`
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <button onclick='document.cookie = "forceDay="+parseInt(prompt("Aktueller Tag"));'>Aktuellen Tag setzen</button>
+                <button onclick='document.cookie = "forceDay= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";'>Zurücksetzen</button>
+            </body>
+        </html>
+        `);
+    } else {
+        res.send("Test Mode is not enabled!");
+    }
+});
 
 export default routes;
